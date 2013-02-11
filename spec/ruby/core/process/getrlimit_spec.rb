8997ab9a44c6d409c524435c4bee4bfb434c70bf
Kenichi Kamiya <kachick1@gmail.com>
2013-02-11 20:57:02 +0900

    Add specs for Process.getrlimit with resource name argument

--- spec/ruby/core/process/getrlimit_spec.rb
+++ spec/ruby/core/process/getrlimit_spec.rb
@@ -4,4 +4,24 @@ require File.expand_path('../../../spec_helper', __FILE__)
 
 describe "Process.getrlimit" do
   it "needs to be reviewed for spec completeness"
+
+  describe "argument is a resource name" do
+    ruby_version_is "" ... "1.9" do
+      it "raises an TypeError" do
+        lambda { Process.getrlimit "CORE" }.should raise_error(TypeError)
+      end
+    end
+
+    ruby_version_is "1.9" do
+      it "returns same value when recieve relative constant" do
+        Process.getrlimit("CORE").should == Process.getrlimit(Process::RLIMIT_CORE)
+      end
+
+      describe "that does not relative to any constant" do
+        it "raises an ArgumentError" do
+          lambda { Process.getrlimit "THIS_IS_NOT_A_CONSTANT" }.should raise_error(ArgumentError)
+        end
+      end
+    end
+  end
 end
