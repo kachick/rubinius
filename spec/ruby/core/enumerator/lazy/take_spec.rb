260f748648b6eaa6b363d6107f16550cc0dafcbe
Kenichi Kamiya <kachick1@gmail.com>
2013-08-06 19:23:50 +0900

    spec lazy#take

--- spec/ruby/core/enumerator/lazy/take_spec.rb
+++ spec/ruby/core/enumerator/lazy/take_spec.rb
@@ -6,7 +6,8 @@ require File.expand_path('../fixtures/classes', __FILE__)
 ruby_version_is "2.0" do
   describe "Enumerator::Lazy#take" do
     before(:each) do
-      @lazy = EnumeratorLazySpecs::MixedYieldAndRaiseError.new.to_enum.lazy
+      @yieldsmixed = EnumeratorLazySpecs::YieldsMixed.new.to_enum.lazy
+      @eventsmixed = EnumeratorLazySpecs::EventsMixed.new.to_enum.lazy
       ScratchPad.record []
     end
 
@@ -15,9 +16,9 @@ ruby_version_is "2.0" do
     end
 
     it "returns new instance of Enumerator::Lazy" do
-      ret = @lazy.take(1)
+      ret = @yieldsmixed.take(1)
       ret.should be_an_instance_of(enumerator_class::Lazy)
-      ret.should_not equal(@lazy)
+      ret.should_not equal(@yieldsmixed)
     end
 
     it "sets given count to size if the given count is less than old size" do
@@ -25,14 +26,13 @@ ruby_version_is "2.0" do
       enumerator_class::Lazy.new(Object.new, 100) {}.take(200).size.should == 100
     end
 
-    it "calls the block only specified times" do
-      @lazy.take(8).force.should == [0, 1, 2, 3, nil, nil, :default_arg, nil]
-      ScratchPad.recorded == []
-    end
+    describe "when the returned Lazy evaluated by .force" do
+      it "stops after specified times" do
+        (0..Float::INFINITY).lazy.take(2).force.should == [0, 1]
 
-    it "ignores an exception when the exception raised after yields" do
-      @lazy.take(9).force.should == [0, 1, 2, 3, nil, nil, :default_arg, nil, [:multiple_yield1, :multiple_yield2]]
-      ScratchPad.recorded == [:after_yields]
+        @eventsmixed.take(1).force
+        ScratchPad.recorded.should == [:before_yield]
+      end
     end
 
     describe "on a nested Lazy" do
@@ -41,8 +41,13 @@ ruby_version_is "2.0" do
         enumerator_class::Lazy.new(Object.new, 100) {}.take(50).take(20).size.should == 20
       end
 
-      it "calls the block only specified times" do
-        @lazy.take(100).take(4).force.should == [0, 1, 2, 3]
+      describe "when the returned Lazy evaluated by .force" do
+        it "stops after specified times" do
+          (0..Float::INFINITY).lazy.map(&:succ).take(2).force.should == [1, 2]
+
+          @eventsmixed.take(10).take(1).force
+          ScratchPad.recorded.should == [:before_yield]
+        end
       end
     end
   end
