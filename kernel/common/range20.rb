a0b2d719e52bf54ab9735c56949e18741f7fd209
Kenichi Kamiya <kachick1@gmail.com>
2013-09-02 23:53:09 +0900

    Implement Range#size

---
+++ kernel/common/range20.rb
@@ -0,0 +1,22 @@
+# -*- encoding: us-ascii -*-
+
+class Range
+  def size
+    case
+    when @end == Float::INFINITY
+      case @begin
+      when Float::INFINITY
+        0
+      when Numeric
+        Float::INFINITY
+      else
+        nil
+      end
+    when @begin.kind_of?(Numeric) && @end.kind_of?(Numeric)
+      diff = @begin - @end
+      diff < 0 ? 0 : diff.to_int
+    else
+      nil
+    end
+  end
+end
