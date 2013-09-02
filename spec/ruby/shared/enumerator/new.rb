243314df2973638043e21f7b1ce8c343a93ff766
Kenichi Kamiya <kachick1@gmail.com>
2013-09-02 21:52:53 +0900

    Add Enumerator.new specs for 2.1

--- spec/ruby/shared/enumerator/new.rb
+++ spec/ruby/shared/enumerator/new.rb
@@ -39,4 +39,74 @@ describe :enum_new, :shared => true do
     enum.to_a.should == [:bar]
   end
 
+  ruby_version_is "2.0" do
+    describe "with size" do
+      before(:each) do
+        ScratchPad.record []
+
+        @proc = Proc.new do
+          ScratchPad << :called
+          :returned_from_call
+        end
+
+        @callable = Object.new
+        def @callable.call
+          ScratchPad << :called
+          :returned_from_call
+        end
+      end
+
+      after(:each) do
+        ScratchPad.clear
+      end
+
+      it "sets the given size to own size if the given size is an Integer" do
+        enumerator_class.new(100) {}.size.should == 100
+      end
+
+      it "sets nil to own size if the given size is nil" do
+        enumerator_class.new(nil) {}.size.should be_nil
+      end
+
+      it "accepts a coercible object after corecing to an Integer" do
+        coercible = mock("coercible to an Integer with #to_int")
+        coercible.should_receive(:to_int).and_return 100
+        enumerator_class.new(coercible) {}.size.should == 100
+      end
+
+      it "accepts a Proc without calling #call in construction" do
+        enum = enumerator_class.new(@proc) {}
+        ScratchPad.recorded.should == []
+        enum.size.should == :returned_from_call
+        ScratchPad.recorded.should == [:called]
+      end
+
+      it "raises a TypeError if the given size is not a callable object" do
+        lambda {
+          enumerator_class.new(Object.new) {}
+        }.should raise_error(TypeError)
+      end
+
+      describe "when the given size is a callable object but not a Proc" do
+        ruby_version_is "" ... "2.1" do
+          it "raises a TypeError without calling #call" do
+            lambda {
+              enumerator_class.new(@callable) {}
+            }.should raise_error(TypeError)
+
+            ScratchPad.recorded.should == []
+          end
+        end
+
+        ruby_version_is "2.1" do
+          it "accepts as a Proc" do
+            enum = enumerator_class.new(@callable) {}
+            ScratchPad.recorded.should == []
+            enum.size.should == :returned_from_call
+            ScratchPad.recorded.should == [:called]
+          end
+        end
+      end
+    end
+  end
 end
