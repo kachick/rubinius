ea0fc8cc0b8ffa2c56540d63de1d74c9671cb8d7
Kenichi Kamiya <kachick1@gmail.com>
2013-09-02 15:27:00 +0900

    Adjust Kernel#{to_enum,enum_for} specs for less than 1.8.7

--- spec/ruby/library/enumerator/to_enum_spec.rb
+++ spec/ruby/library/enumerator/to_enum_spec.rb
@@ -1,10 +1,10 @@
 require File.expand_path('../../../spec_helper', __FILE__)
 
-ruby_version_is ""..."1.9" do
-  require File.expand_path('../../../shared/enumerator/enum_for', __FILE__)
+ruby_version_is ""..."1.8.7" do
   require 'enumerator'
+  require File.expand_path('../../../shared/enumerator/enum_for', __FILE__)
 
-  describe "#to_enum" do
+  describe "Kernel#to_enum" do
     it_behaves_like :enum_for, :to_enum
   end
 end
